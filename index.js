6b326ff541b4b22e8e191458a416b1119642a668
Handla <80836500+Phransis@users.noreply.github.com>
2023-04-05 14:46:43 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import Register from './userRegistry/Register/Register';
+// import Register from './userRegistry/Register/Register'
+ import Login from './userRegistry/Login/Login';
+import Welcome from './userRegistry/Welcome/Welcome';
+//import Welcome from './userRegistry/Welcome/Welcome';
+
+//import background from '../../images/login2.jpg'
+
+
+
+
+
+
+
+
+function Index() {
+  return (
+    <>
+        <section>
+            <Welcome/>
+        </section>
+    </>
+  );
+}
+
+export default Index;
