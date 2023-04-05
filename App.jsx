e004cbeb14bcfca15c09ff595197b5f7f0225924
Handla <80836500+Phransis@users.noreply.github.com>
2023-04-05 14:47:30 +0000

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,20 @@
+
+//import React, { useState } from 'react'
+import Index from './components/Index'
+//import {BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
+
+
+function App() {
+
+//const [currentForm, setCurrentForm] = useState('Login')
+
+  return (
+    <>
+      <main>
+        <Index/>
+      </main>
+    </>
+  );
+}
+
+export default App;
